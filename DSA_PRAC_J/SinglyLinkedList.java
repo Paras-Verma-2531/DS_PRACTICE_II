29434bdd49e0396864c3c7fe141309d04a370302
paras <parasverma2531@gmail.com>
2022-06-15 22:39:01 +0530

    method implemented

--- DSA_PRAC_J/SinglyLinkedList.java
+++ DSA_PRAC_J/SinglyLinkedList.java
@@ -1,5 +1,4 @@
 package DSA_PRAC_J;
-
 public class SinglyLinkedList {
     protected Node head=null;protected Node temp;
 
@@ -40,19 +39,44 @@ public class SinglyLinkedList {
             node.next=temp.next;
             temp.next=node;
         }
-
-
     }
     public int deleteFromBeg()
     {
-
+         int data=-1;
+         if(head!=null)
+         {Node.length--;
+             data=head.data;
+             head=head.next;
+         }return data;
     }
     public int deleteFromEnd()
     {
-
+         int data=-1;
+         temp=head;
+         if(head!=null) {
+             Node.length--;
+             while (temp.next.next != null)
+                 temp = temp.next;
+             data = temp.next.data;
+             temp.next = null;
+         }return data;
     }
     public int deleteFromPos(int pos)
-    {
-
+    { int data=-1;
+          if(pos>Node.length||pos<1)
+              System.out.println("invalid position");
+          if(pos==1)
+              data=deleteFromBeg();
+          if(pos==Node.length)
+              data=deleteFromEnd();
+          else
+          {Node.length--;
+              int i=1;
+              temp=head;
+              while(i<pos-1)
+                  temp=temp.next;
+              data=temp.next.data;
+              temp.next=temp.next.next;
+          }return data;
     }
 }
